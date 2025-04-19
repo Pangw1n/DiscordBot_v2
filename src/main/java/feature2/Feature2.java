85a88d07b0c574cdad14a69f3b1cc6390b228af7
league <league@imac-18.attlocal.net>
2025-04-18 18:32:11 -0700

    working on verifying moves

--- src/main/java/feature2/Feature2.java
+++ src/main/java/feature2/Feature2.java
@@ -26,7 +26,19 @@ public class Feature2 extends Feature
 		else if (messageContent.contains(COMMAND))
 		{
 			String[] command = messageContent.replace(COMMAND, "").trim().split(" ");
-			if (command.length == 3)
+			if (command.length == 1)
+			{
+				String from = command[0];
+				setPiece(from, "", "");
+				event.sendResponse(getBoard());
+			}
+			if (command.length == 2)
+			{
+				String from = command[0];
+				String to = command[1];
+				event.sendResponse(movePiece(from, to));
+			}
+			else if (command.length == 3)
 			{
 				String from = command[0];
 				String toColor = command[1];
@@ -34,31 +46,32 @@ public class Feature2 extends Feature
 				setPiece(from, toColor, toPiece);
 				event.sendResponse(getBoard());
 			}
-			else if (command.length == 2)
-			{
-				String from = command[0];
-				String to = command[1];
-				movePiece(from, to);
-				event.sendResponse(getBoard());
-			}
 		}
 	}
 	
-	private void movePiece(String from, String to) {
+	private String movePiece(String from, String to) {
 		// TODO Auto-generated method stub
 		int fromCol = from.charAt(0) - 'a';
-		int fromRow = from.charAt(0) - '1';
-		int toCol = from.charAt(0) - 'a';
-		int toRow = from.charAt(0) - '1';
+		int fromRow = from.charAt(1) - '1';
+		int toCol = to.charAt(0) - 'a';
+		int toRow = to.charAt(1) - '1';
 		
-		board[toRow][toCol] = board[fromRow][fromCol];
+		if (fromCol >= 8 || fromRow >= 8 || toCol >= 8 || toRow >= 8)
+			return "Out of bounds";
+		if (board[toRow][toCol].getColor() == board[fromRow][fromCol].getColor())
+			return "You cannot take a piece of the same color";
+		Piece temp = board[fromRow][fromCol];
 		board[fromRow][fromCol] = new Piece(PieceType.EMPTY, Color.EMPTY);
+		board[toRow][toCol] = temp;
+		return getBoard();
 	}
 	
 	private void setPiece(String from, String toColor, String toPiece)
 	{
 		int fromCol = from.charAt(0) - 'a';
-		int fromRow = from.charAt(0) - '1';
+		int fromRow = from.charAt(1) - '1';
+		if (fromCol >= 8 || fromRow >= 8)
+			return;
 		Color color;
 		switch (toColor.toLowerCase().trim())
 		{
@@ -90,6 +103,9 @@ public class Feature2 extends Feature
 			case "bishop":
 				type = PieceType.BISHOP;
 				break;
+			case "pawn":
+				type = PieceType.PAWN;
+				break;
 			default:
 				type = PieceType.EMPTY;
 		}
@@ -98,11 +114,10 @@ public class Feature2 extends Feature
 	}
 
 	private String getBoard() {
-		System.out.println("get board");
 		String result = "```  a b c d e f g h\n";
-		for (int r = 0; r < 8; r++)
+		for (int r = 7; r >= 0; r--)
 		{
-			result += (8 - r) + " ";
+			result += (r + 1) + " ";
 			for (int c = 0; c < 8; c++)
 			{
 				Color color = board[r][c].getColor();
@@ -153,13 +168,11 @@ public class Feature2 extends Feature
 			result += "\n";
 		}
 		result += "```";
-		System.out.println(result);
 		return result;
 	}
 
 	public void resetBoard()
 	{
-		System.out.println("reset board");
 		board = new Piece[8][8];
 		
 		for (int r = 0; r < board.length; r++)
@@ -170,43 +183,80 @@ public class Feature2 extends Feature
 			}
 		}
 		
-		board[0][0] = new Piece(PieceType.ROOK, Color.BLACK);
-		board[0][1] = new Piece(PieceType.KNIGHT, Color.BLACK);
-		board[0][2] = new Piece(PieceType.BISHOP, Color.BLACK);
-		board[0][3] = new Piece(PieceType.QUEEN, Color.BLACK);
-		board[0][4] = new Piece(PieceType.KING, Color.BLACK);
-		board[0][5] = new Piece(PieceType.BISHOP, Color.BLACK);
-		board[0][6] = new Piece(PieceType.KNIGHT, Color.BLACK);
-		board[0][7] = new Piece(PieceType.ROOK, Color.BLACK);
+		board[7][0] = new Piece(PieceType.ROOK, Color.BLACK);
+		board[7][1] = new Piece(PieceType.KNIGHT, Color.BLACK);
+		board[7][2] = new Piece(PieceType.BISHOP, Color.BLACK);
+		board[7][3] = new Piece(PieceType.QUEEN, Color.BLACK);
+		board[7][4] = new Piece(PieceType.KING, Color.BLACK);
+		board[7][5] = new Piece(PieceType.BISHOP, Color.BLACK);
+		board[7][6] = new Piece(PieceType.KNIGHT, Color.BLACK);
+		board[7][7] = new Piece(PieceType.ROOK, Color.BLACK);
 
-		board[1][0] = new Piece(PieceType.PAWN, Color.BLACK);
-		board[1][1] = new Piece(PieceType.PAWN, Color.BLACK);
-		board[1][2] = new Piece(PieceType.PAWN, Color.BLACK);
-		board[1][3] = new Piece(PieceType.PAWN, Color.BLACK);
-		board[1][4] = new Piece(PieceType.PAWN, Color.BLACK);
-		board[1][5] = new Piece(PieceType.PAWN, Color.BLACK);
-		board[1][6] = new Piece(PieceType.PAWN, Color.BLACK);
-		board[1][7] = new Piece(PieceType.PAWN, Color.BLACK);
+		board[6][0] = new Piece(PieceType.PAWN, Color.BLACK);
+		board[6][1] = new Piece(PieceType.PAWN, Color.BLACK);
+		board[6][2] = new Piece(PieceType.PAWN, Color.BLACK);
+		board[6][3] = new Piece(PieceType.PAWN, Color.BLACK);
+		board[6][4] = new Piece(PieceType.PAWN, Color.BLACK);
+		board[6][5] = new Piece(PieceType.PAWN, Color.BLACK);
+		board[6][6] = new Piece(PieceType.PAWN, Color.BLACK);
+		board[6][7] = new Piece(PieceType.PAWN, Color.BLACK);
 		
 
 		
-		board[7][0] = new Piece(PieceType.ROOK, Color.WHITE);
-		board[7][1] = new Piece(PieceType.KNIGHT, Color.WHITE);
-		board[7][2] = new Piece(PieceType.BISHOP, Color.WHITE);
-		board[7][3] = new Piece(PieceType.QUEEN, Color.WHITE);
-		board[7][4] = new Piece(PieceType.KING, Color.WHITE);
-		board[7][5] = new Piece(PieceType.BISHOP, Color.WHITE);
-		board[7][6] = new Piece(PieceType.KNIGHT, Color.WHITE);
-		board[7][7] = new Piece(PieceType.ROOK, Color.WHITE);
+		board[0][0] = new Piece(PieceType.ROOK, Color.WHITE);
+		board[0][1] = new Piece(PieceType.KNIGHT, Color.WHITE);
+		board[0][2] = new Piece(PieceType.BISHOP, Color.WHITE);
+		board[0][3] = new Piece(PieceType.QUEEN, Color.WHITE);
+		board[0][4] = new Piece(PieceType.KING, Color.WHITE);
+		board[0][5] = new Piece(PieceType.BISHOP, Color.WHITE);
+		board[0][6] = new Piece(PieceType.KNIGHT, Color.WHITE);
+		board[0][7] = new Piece(PieceType.ROOK, Color.WHITE);
 
-		board[6][0] = new Piece(PieceType.PAWN, Color.WHITE);
-		board[6][1] = new Piece(PieceType.PAWN, Color.WHITE);
-		board[6][2] = new Piece(PieceType.PAWN, Color.WHITE);
-		board[6][3] = new Piece(PieceType.PAWN, Color.WHITE);
-		board[6][4] = new Piece(PieceType.PAWN, Color.WHITE);
-		board[6][5] = new Piece(PieceType.PAWN, Color.WHITE);
-		board[6][6] = new Piece(PieceType.PAWN, Color.WHITE);
-		board[6][7] = new Piece(PieceType.PAWN, Color.WHITE);
+		board[1][0] = new Piece(PieceType.PAWN, Color.WHITE);
+		board[1][1] = new Piece(PieceType.PAWN, Color.WHITE);
+		board[1][2] = new Piece(PieceType.PAWN, Color.WHITE);
+		board[1][3] = new Piece(PieceType.PAWN, Color.WHITE);
+		board[1][4] = new Piece(PieceType.PAWN, Color.WHITE);
+		board[1][5] = new Piece(PieceType.PAWN, Color.WHITE);
+		board[1][6] = new Piece(PieceType.PAWN, Color.WHITE);
+		board[1][7] = new Piece(PieceType.PAWN, Color.WHITE);
 	}
-
+	
+	
+	private boolean isLegal(Piece piece, int fromRow, int fromCol, int toRow, int toCol)
+	{
+		switch (piece.getType())
+		{
+			case PAWN:
+				int direction = (piece.getColor() == Color.WHITE) ? 1 : -1;
+				if (toRow - fromRow == direction)
+				{
+					if (Math.abs(toCol - fromCol) == 1 && board[toRow][toCol].getColor() != Color.EMPTY)
+					{
+						return false;
+					}
+					else if (toCol == fromCol && board[toRow][toCol].getColor() == Color.EMPTY)
+					{
+						return true;
+					}
+				}
+				else if (piece.isFirstMove())
+				{
+					
+				}
+				return false;
+			case KING:
+				return false;
+			case QUEEN:
+				return false;
+			case ROOK:
+				return false;
+			case KNIGHT:
+				return false;
+			case BISHOP:
+				return false;
+			default:
+				return false;
+		}
+	}
 }
--- src/main/java/feature2/Piece.java
+++ src/main/java/feature2/Piece.java
@@ -3,10 +3,18 @@ package feature2;
 public class Piece {
 	private PieceType type;
 	private Color color;
+	private boolean firstMove;
 	
 	public Piece(PieceType type, Color color) {
-		this.type = type;
-		this.color = color;
+		if (type == PieceType.EMPTY || color == Color.EMPTY) {
+			this.type = PieceType.EMPTY;
+			this.color = Color.EMPTY;
+		}
+		else {
+			this.type = type;
+			this.color = color;
+		}
+		firstMove = true;
 	}
 	
 	public PieceType getType()
@@ -19,8 +27,13 @@ public class Piece {
 		return color;
 	}
 	
-	public void setType(PieceType type)
+	public boolean isFirstMove()
 	{
-		this.type = type;
+		return firstMove;
+	}
+	
+	public void moved()
+	{
+		firstMove = false;
 	}
 }
